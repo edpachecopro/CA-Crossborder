63ff6c93d18f5a2e15af7214968990e35f915270
Edgard Pacheco <72029558+edpachecopro@users.noreply.github.com>
2023-05-02 21:12:11 +0100

    adding files

---
+++ src/main/java/net/javaguides/springboot/mainCrossborder.java
@@ -0,0 +1,13 @@
+package net.javaguides.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class mainCrossborder {
+
+	public static void main(String[] args) {
+		SpringApplication.run(mainCrossborder.class, args);
+	}
+
+}
